1fc248efc8957e4e075cf3af5232f50474ce2d39
kareem886 <73999071+kareem886@users.noreply.github.com>
2026-09-19 16:37:48 +0300

    Update app_whatsapp.py

--- app_whatsapp.py
+++ app_whatsapp.py
@@ -21,6 +21,7 @@ PHONE_NUMBER_ID = os.environ.get("PHONE_NUMBER_ID", "1358537447338280")
 ACCESS_TOKEN    = os.environ.get("ACCESS_TOKEN", "")
 VERIFY_TOKEN    = os.environ.get("VERIFY_TOKEN", "schoolai2026")
 APP_SECRET      = os.environ.get("APP_SECRET", "")
+ADMIN_USER      = "admin"
 ADMIN_PASSWORD  = "moderninfinity2026"
 META_API_URL    = f"https://graph.facebook.com/v18.0/{PHONE_NUMBER_ID}/messages"
 
